936a9386cdfa619ac4169f60f36fa9ac10c00627
v-e-r-s-u-s <dwalter.engineer@gmail.com>
2023-06-25 23:08:43 -0500

    Cleanup

--- src/main/java/dw0623/App.java
+++ src/main/java/dw0623/App.java
@@ -2,11 +2,6 @@ package dw0623;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
     public RentalAgreement checkout(String toolCode, String checkoutDate,
         Integer rentalDays, Integer discount) throws Exception {
 
